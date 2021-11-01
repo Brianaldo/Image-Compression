17821c5bfbda92e2ac269df32301304686174635
Brianaldo <brianaldophandiarta31@gmail.com>
2021-11-01 10:22:56 +0700

    add ImageCanvas Component

---
+++ src/frontend/src/Components/ImageCanvas/ImageCanvas.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useRef } from "react";
+import { render } from "react";
+
+import "./ImageCanvas.css";
+
+function getCurrentFrame(index) {
+  //   return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+  //     .toString()
+  //     .padStart(4, "0")}.jpg`;
+  // return `${imgLink[index]}`;
+  return require(`./img/img_${index.toString().padStart(3, "0")}.jpg`).default;
+}
+
+const ImageCanvas = ({ scrollHeight, numFrames, width, height }) => {
+  const canvasRef = useRef(null);
+  const [images, setImages] = useState([]);
+  const [frameIndex, setFrameIndex] = useState(0);
+
+  function preloadImages() {
+    for (let i = 1; i <= numFrames; i++) {
+      const img = new Image();
+      const imgSrc = getCurrentFrame(i);
+      img.src = imgSrc;
+      setImages((prevImages) => [...prevImages, img]);
+    }
+  }
+
+  const handleScroll = () => {
+    const scrollFraction = window.scrollY / (scrollHeight - window.innerHeight);
+    const index = Math.min(
+      numFrames - 1,
+      Math.ceil(scrollFraction * numFrames)
+    );
+
+    if (index <= 0 || index > numFrames) {
+      return;
+    }
+
+    setFrameIndex(index);
+  };
+
+  const renderCanvas = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.canvas.width = width;
+    context.canvas.height = height;
+  };
+
+  useEffect(() => {
+    preloadImages();
+    renderCanvas();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current || images.length < 1) {
+      return;
+    }
+
+    const context = canvasRef.current.getContext("2d");
+    let requestId;
+
+    const render = () => {
+      context.drawImage(images[frameIndex], 0, 0);
+      requestId = requestAnimationFrame(render);
+    };
+
+    render();
+
+    return () => cancelAnimationFrame(requestId);
+  }, [frameIndex, images]);
+
+  return (
+    <div style={{ height: scrollHeight }}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
+};
+
+export default ImageCanvas;
