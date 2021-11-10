89a845847011e3db5e3fbb3a86719ed24ffb1543
Brianaldo <brianaldophandiarta31@gmail.com>
2021-11-10 09:08:35 +0700

    edit App

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,19 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import ImageCanvas from "./Components/ImageCanvas/ImageCanvas";
+import MainPage from "./Components/MainPage/MainPage";
 
 function App() {
+  console.log(window.innerWidth);
   return (
     <div className="App">
       <ImageCanvas
-        scrollHeight={4000}
-        width={1440}
+        scrollHeight={10000}
+        width={1920}
         height={1080}
-        numFrames={299}
+        numFrames={775}
       />
+      <MainPage/>
     </div>
   );
 }
